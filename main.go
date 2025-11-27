02cfb1a8b0987502eefe13cd613d662dd799d869
maczilla <bogdan.d@outlook.com>
2025-11-27 13:25:27 +0100

    added a hits tracker ( not persistent) and handlers to see the metrics and reset them

--- main.go
+++ main.go
@@ -1,23 +1,30 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"sync/atomic"
 )
 
-
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
 
 func main() {
 	const port = "8080"
 	const filePathRoot = "."
+	apiCfg := &apiConfig{}
 
 	mux := http.NewServeMux()
 
 	fs := http.FileServer(http.Dir(filePathRoot))
-	mux.Handle("/app/", http.StripPrefix("/app", fs))
+	mux.Handle("/app/", http.StripPrefix("/app", apiCfg.middlewareMetricsInc(fs)))
 
 
 	mux.HandleFunc("/healthz", handlerReadiness)
+	mux.HandleFunc("/metrics", apiCfg.handlerMetrics)
+	mux.HandleFunc("/reset", apiCfg.handlerReset)
 
 	server := &http.Server{
 		Addr: ":" + port,
@@ -33,4 +40,26 @@ func handlerReadiness(w http.ResponseWriter, r *http.Request) {
 	w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 	w.WriteHeader(http.StatusOK)
 	w.Write([]byte(http.StatusText(http.StatusOK)))
+}
+
+func (cfg *apiConfig) handlerMetrics(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type",  "text/plain; charset=utf-8")
+	count := cfg.fileserverHits.Load()
+	fmt.Fprintf(w, "Hits: %d", count)
+}
+
+func (cfg *apiConfig) handlerReset(w http.ResponseWriter, r *http.Request) {
+	cfg.fileserverHits.Store(0)
+	w.Header().Set("Content-Type",  "text/plain; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	fmt.Fprint(w, "Hits reset to 0!")
+
+}
+
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
 }
